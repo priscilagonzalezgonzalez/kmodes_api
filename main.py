0c6767a7efcf2ad8e7d09176a8db0b415429ae7d
Priscila González <psarahi.gonzalez@alumnos.udg.mx>
2024-04-16 14:36:47 -0600

    Se modifica main para poder ejecutar la aplicacion flask

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify, request
 from conexion import get_prediction, train_model
 
-app = Flask(__name__)
-# Initialize Kmodes
-train_model()
 
-@app.route("/api/v1/predictions/<int:id>", methods=["GET"])
-def predict(id):
-    return jsonify(get_prediction(id))
+def start_app():
+    app = Flask(__name__)
+    # Initialize Kmodes
+    train_model()
+
+    @app.route("/api/v1/predictions/<int:id>", methods=["GET"])
+    def predict(id):
+        return jsonify(get_prediction(id))
+    
+    return app
+
+if __name__ == "__main__":
+    app =  start_app()
+    app.run()
