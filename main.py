04a18fc8d24feab57d1c2c88ae55ce51155032df
Priscila González <psarahi.gonzalez@alumnos.udg.mx>
2024-04-16 17:32:40 -0600

    CORS

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from conexion import get_prediction, train_model
 
 
 def start_app():
     app = Flask(__name__)
+    CORS(app)
     # Initialize Kmodes
     train_model()
 
